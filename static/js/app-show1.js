84172dba6353dc9d729f9c82a252fa2fd398e251
zileyuan <zileyuan@gmail.com>
2016-03-11 11:00:57 +0800

    add xiaoji、heji

--- static/js/app-show1.js
+++ static/js/app-show1.js
@@ -228,15 +228,39 @@ webix.ui({
     ]
 });
 
+webix.ui.datafilter.pageSummColumn = webix.extend({
+    refresh:function(master, node, value){
+        var result = 0;
+        var page = master.getPage();
+        var pager = master.getPager();
+        var cc = pager.data.size;
+        var iid = master.getIdByIndex(page * cc);
+        master.mapCells(iid, value.columnId, cc, 1, function(value){
+            value = value*1;
+            if (!isNaN(value))
+                result+=value;
+            return value;
+        });
+
+        node.firstChild.innerHTML = Math.round(result);
+    }
+}, webix.ui.datafilter.summColumn);
+
 grida = webix.ui({
     container:"testA",
     view:"datatable",
     id: "dtable",
     columns:[
-        { id:"huiyk_id",	header:"卡号"},
+        { id:"huiyk_id",	header:"卡号", footer:[
+            { height:40, text:"xiaoji", colspan:3 },
+            { height:80, text:"heji", colspan:3 }
+        ]},
         { id:"huiyk_germc",	header:"持卡人"},
         { id:"name",	header:"卡类型"},
-        { id:"chuz",	header:"本期储值"},
+        { id:"chuz",	header:"本期储值", footer:[
+            { content:"pageSummColumn" },
+            { content:"summColumn" },
+        ]},
         { id:"xiaof",	header:"本期消费"},
         { id:"zxye",	header:"期初金额"},
         { id:"zdye",	header:"本期余额"},
@@ -248,6 +272,7 @@ grida = webix.ui({
     ],
     select:"cell",
     autowidth:true,
+    footer:true,
     pager:{
         template:"{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()}",
         container:"paging_here",
